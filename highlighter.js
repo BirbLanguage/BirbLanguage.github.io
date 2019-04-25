17639fd019538e2d3d3fb3215c66f9adeee48a20
Seniru Pasan Indira <34127015+Seniru@users.noreply.github.com>
2019-04-25 14:30:47 +0530

    Bug Fixes
    Fixed the highlighting of numbers when they are inside quotes or commented

--- highlighter.js
+++ highlighter.js
@@ -4,28 +4,22 @@ const bitwise = ['or', 'and', 'not', 'floofy', 'floofier', 'as', 'than'];
 
 function highlight(code) {
     
-    let new_code = code;
+    let new_code = code.replace(/<br>/g, '<br />' );
 
-    //highlighting numbers
-    for (let n=0; n<=9; n++) {
-        //new_code = new_code.replace(new RegExp(" (?<!<.*>)"+n+"(?!<\/.*>)", 'g'), '<num>'+n+'</num>');
-        new_code = new_code.replace(new RegExp(n, 'g'), '<num>'+n+'</num>');
-        console.log(new_code)
-    }
-
-        //highlighting strings 
-        const strMatches = new_code.match(/[\"\'].*[\"\']/g);
-        if(strMatches!=null&&strMatches.length!=0) {
-            for (let match of strMatches) {
-                new_code = new_code.replace(new RegExp(match,"g"),"<string>"+match+"</string>");
-             }
+    
+    //highlighting strings 
+    const strMatches = new_code.match(/[\"\'].*[\"\']/g);
+    if(strMatches!=null&&strMatches.length!=0) {
+        for (let match of strMatches) {
+            new_code = new_code.replace(new RegExp(match,"g"),"<string>"+match+"</string>");
         }
+    }
     
     
-
+    
     //highlighting comments
-    new_code = new_code.replace(/#/gi, "<comment>#").replace(/(?<=#.*)<br>/gi,'</comment><br>');
-
+    new_code = new_code.replace(/#/gi, "<comment>#").replace(/(?<=#.*)<br \/>/gi,'</comment><br />');
+    
     //highlighting keywords, math words and bitwise operations
     for (let key of keywords) {
         new_code = new_code.replace(new RegExp(key, 'g'), "<key>"+key+'</key>');
@@ -34,12 +28,18 @@ function highlight(code) {
     for (let key of bitwise) {
         new_code = new_code.replace(new RegExp(key, 'g'), "<bitwise>"+key+'</bitwise>');
     }
-
+    
     for (let key of math_words) {
         new_code = new_code.replace(new RegExp(key, 'g'), "<math>"+key+'</math>');
     }
     
-
+    //highlighting numbers
+    for (let n=0; n<=9; n++) {
+        //new_code = new_code.replace(new RegExp(" (?<!<.*>)"+n+"(?!<\/.*>)", 'g'), '<num>'+n+'</num>');
+        new_code = new_code.replace(new RegExp("((?<=<.*>.*<\/.*>.*)|(?<!<.*>.*))"+n+"((?!.*<\/.*>)|(?=<.*>.*<\/.*>.*))", 'gi'), '<num>'+n+'</num>');
+        console.log(new_code)
+    }
+    
     return new_code;
 
 }
