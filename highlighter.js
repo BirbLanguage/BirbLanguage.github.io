646f8ab596451ac412fa929b1ba6512fb561e877
Seniru Pasan Indira <senirupasan@gmail.com>
2019-04-26 23:28:32 +0530

    Add syntax highlighting for codes (#2)
    * Basic support for syntax highlighting
    
    * Bug Fixes
    
    Fixed the highlighting of numbers when they are inside quotes or commented
    
    * Rewrited the highlighter
    
    * Rewrited the highlighter so it doesn't have bugs that were found in the previous  version.
    
    *Reduced the size of the code and increase the efficieny
    
    * Fixed a typo

---
+++ highlighter.js
@@ -0,0 +1,24 @@
+const comment = /#.*/g;
+const strDblQuoted = /(?<!#.*)\".*\"/g;
+const strSingleQuoted = /(?<!#.*)\'.*\'/g;
+const nums = /(?<!([a-zA-Z])|#.*|[\"\'])\d(?![a-zA-Z])/g;
+const keywords = /(?<!([a-zA-Z])|#.*|[\"\'].*)(hatch egg|slep|squawk|chirp|new birb|is flying while|is|now|peck|feed|just|desires seed|eat seed if|throw seed away|no longer desires|stop flying)(?![a-zA-Z])/g; 
+const bitwise = /(?<!([a-zA-Z])|#.*|[\"\'])(or|and|not|floofy|floofier|as|than)(?![a-zA-Z])/g;
+const math_words = /(?<!([a-zA-Z])|#.*|[\"\'])(floof|unfloof|megafloof|megaunfloof|ultrafloof|ultraunfloof|by)(?![a-zA-Z])/g;
+
+function highlight(code) {
+    
+    let new_code = code;
+
+    new_code = new_code.replace(keywords, '<key>$&</key>') //highlighting keywords
+                        .replace(strDblQuoted, '<string>\"$&\"</string>')     // highlighting strings
+                        .replace(strSingleQuoted, "<string>\'$&\'</string>")
+                        .replace(comment, '<comment>$&</comment>') //highlighting comments
+                        .replace(nums, '<num>$&</num>')     //highlighting numbers
+                        .replace(bitwise, '<bitwise>$&</bitwise>')  //highlighting bitwise operators
+                        .replace(math_words, '<math>$&</math>'); //highlighting keywords of mathematical expressions
+                             
+    return new_code.replace(/\n/g,'<br>')
+    ;
+
+}
